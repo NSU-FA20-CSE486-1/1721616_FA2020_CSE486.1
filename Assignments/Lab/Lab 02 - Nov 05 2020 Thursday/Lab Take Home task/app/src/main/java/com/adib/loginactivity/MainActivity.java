1bccac8304c6ae493f893f88af88f786200a1067
JubaerKhan <jubaer.khan@northsouth.edu>
2020-11-07 02:41:21 +0600

    Lab 02 take home task #5
    Lab two task is all about log in activity.

---
+++ Assignments/Lab/Lab 02 - Nov 05 2020 Thursday/Lab Take Home task/app/src/main/java/com/adib/loginactivity/MainActivity.java
@@ -0,0 +1,51 @@
+package com.adib.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText eEmailAddress, ePassword;
+    private TextView forgotPassword;
+    private Button btnSignin;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        eEmailAddress = (EditText) findViewById(R.id.eEmailAddress);
+        ePassword = (EditText) findViewById(R.id.ePassword);
+        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        btnSignin = (Button)  findViewById(R.id.btnSignin);
+
+
+
+
+    }
+    public void signIn(View view) {
+        if(eEmailAddress .getText().toString().equals("adib") &&
+                ePassword.getText().toString().equals("123456")) {
+            Toast.makeText(getApplicationContext(), "Welcome to our world!",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "Wrong  Credentials",Toast.LENGTH_SHORT).show();
+
+
+        }
+
+
+    }
+    public void forgotPassword(View view) {
+
+        Toast.makeText(getApplicationContext(), "This feature is not available",Toast.LENGTH_SHORT).show();
+
+    }
+}
