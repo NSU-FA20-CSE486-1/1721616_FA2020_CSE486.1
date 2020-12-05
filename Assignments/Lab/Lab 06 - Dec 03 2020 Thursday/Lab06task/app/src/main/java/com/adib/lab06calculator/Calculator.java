8efc8f0566ffc51502cabda17a38b66a039615e6
JubaerKhan <jubaer.khan@northsouth.edu>
2020-12-05 14:24:14 +0600

    Lab task 06 #12
    Coplete the given task for lab 06

---
+++ Assignments/Lab/Lab 06 - Dec 03 2020 Thursday/Lab06task/app/src/main/java/com/adib/lab06calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.adib.lab06calculator;
+
+
+public class Calculator {
+
+    public enum Operator {ADD, SUB, DIV, MUL, POW}
+
+    public double addition(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public double subtraction(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    public double division(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+
+    public double multiplication(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+
+    public double power(double firstOperand, double secondOperand) {
+        return Math.pow(firstOperand, secondOperand);
+    }
+}
