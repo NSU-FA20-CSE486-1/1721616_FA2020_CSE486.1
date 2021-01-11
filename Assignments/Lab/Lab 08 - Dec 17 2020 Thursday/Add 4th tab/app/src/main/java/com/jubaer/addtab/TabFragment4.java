734012265a0bede79f4ec9f49b00b4b5dcdbf4b1
JubaerKhan <jubaer.khan@northsouth.edu>
2021-01-11 21:22:44 +0600

    Lab task 08 #20
    Add 4 tab and show their body

---
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/Add 4th tab/app/src/main/java/com/jubaer/addtab/TabFragment4.java
@@ -0,0 +1,25 @@
+package com.jubaer.addtab;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TabFragment4 extends Fragment {
+
+
+    public TabFragment4() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_fragment4, container, false);
+    }
+
+}
