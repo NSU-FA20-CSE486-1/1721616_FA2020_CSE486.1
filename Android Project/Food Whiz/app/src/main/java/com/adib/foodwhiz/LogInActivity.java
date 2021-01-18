ea18b1c8919cc84432c78851c9aaf9c256474766
JubaerKhan <jubaer.khan@northsouth.edu>
2021-01-18 15:15:12 +0600

    SignIn #11
    Sign up is compleated but have faceing issue with connecting data base.

--- Android Project/Food Whiz/app/src/main/java/com/adib/foodwhiz/LogInActivity.java
+++ Android Project/Food Whiz/app/src/main/java/com/adib/foodwhiz/LogInActivity.java
@@ -2,7 +2,9 @@ package com.adib.foodwhiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void signUp(View view) {
+        Intent reg = new Intent(LogInActivity.this, SignUpActivity.class);
+        startActivity(reg);
+        finish();
+    }
+
+    public void forgotPassword(View view) {
     }
 }
