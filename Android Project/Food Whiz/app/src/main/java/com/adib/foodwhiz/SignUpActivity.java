3d67b624e72e722c12bfac4d843e78cc3ad9738a
JubaerKhan <jubaer.khan@northsouth.edu>
2021-01-18 17:32:56 +0600

    SignUp activity #24 #11
    Updated sign up activity with scroll view

--- Android Project/Food Whiz/app/src/main/java/com/adib/foodwhiz/SignUpActivity.java
+++ Android Project/Food Whiz/app/src/main/java/com/adib/foodwhiz/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.adib.foodwhiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void alreadyHaveAccount(View view) {
+        Intent dsp = new Intent(SignUpActivity.this,LogInActivity.class);
+        startActivity(dsp);
+        finish();
     }
 }
